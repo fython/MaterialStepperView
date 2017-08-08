a54edd0a8ce31acf4083889a729ecadf00f77b35
Fung <fython@163.com>
2017-08-09 00:35:13 +0800

    MainActivity: Add some menu options
    Signed-off-by: Fung <fython@163.com>

--- demo/src/main/java/moe/feng/common/stepperview/demo/MainActivity.java
+++ demo/src/main/java/moe/feng/common/stepperview/demo/MainActivity.java
@@ -1,6 +1,10 @@
 package moe.feng.common.stepperview.demo;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.customtabs.CustomTabsIntent;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
@@ -9,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import moe.feng.alipay.zerosdk.AlipayZeroSdk;
 import moe.feng.common.stepperview.demo.fragment.VerticalStepperAdapterDemoFragment;
 import moe.feng.common.stepperview.demo.fragment.VerticalStepperDemoFragment;
 
@@ -68,9 +73,35 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 				getSupportFragmentManager().beginTransaction()
 						.replace(R.id.container, mVerticalStepperAdapterDemoFragment).commit();
 				return true;
+			case R.id.action_alipay_donate:
+				if (AlipayZeroSdk.hasInstalledAlipayClient(this)) {
+					AlipayZeroSdk.startAlipayClient(this, "aehvyvf4taua18zo6e");
+				} else {
+					new AlertDialog.Builder(this)
+							.setTitle(R.string.donate_dialog_title)
+							.setMessage(R.string.donate_dialog_message)
+							.setPositiveButton(android.R.string.ok, null)
+							.setNeutralButton(R.string.doante_dialog_paypal_button, new DialogInterface.OnClickListener() {
+								@Override
+								public void onClick(DialogInterface dialogInterface, int i) {
+									openWebsite("https://paypal.me/fython");
+								}
+							})
+							.show();
+				}
+				return true;
+			case R.id.action_fork_on_github:
+				openWebsite("https://github.com/fython/MaterialStepperView");
+				return true;
 			default:
 				return false;
 		}
+	}
+
+	private void openWebsite(String url) {
+		CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+		builder.setToolbarColor(getResources().getColor(R.color.colorPrimary));
+		builder.build().launchUrl(this, Uri.parse(url));
 	}
 
 }
